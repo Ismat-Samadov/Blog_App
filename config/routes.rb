4e8f31b4119e2d0d82c574222c2f8242c7fd0416
Ismat-Samadov <ismetsemedov@gmail.com>
2024-04-25 11:11:03 +0400

    check

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :blogs
   root 'blogs#index'
+
+  resources :articles
+  # If you want articles#index to be the root
+  # root 'articles#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,3 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Article.create(title: 'First Article', body: 'This is the body of the first article.')
+Article.create(title: 'Second Article', body: 'This is the body of the second article.')
+Article.create(title: 'Third Article', body: 'This is the body of the third article.')
