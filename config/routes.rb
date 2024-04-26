2e327ef3a97fb49f168eedb25a1097c4d25335e0
Ismat-Samadov <ismetsemedov@gmail.com>
2024-04-26 19:33:59 +0400

    check

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resources :blogs
-  root 'blogs#index'
-
   resources :articles
-  # If you want articles#index to be the root
-  # root 'articles#index'
+
+  root 'articles#index'
 end
